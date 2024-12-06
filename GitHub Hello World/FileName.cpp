c8d2dd2d18d708ab8e1850107aad28c075e447ee
Anthony Catalano <acatalano212@outlook.com>
2024-12-05 19:10:39 -0500

    Updating Main

--- GitHub Hello World/FileName.cpp
+++ GitHub Hello World/FileName.cpp
@@ -5,10 +5,13 @@ using namespace std;
 
 int main() {
 
-	string name;
+	string name, ans;
 	cout << "What is your name?" << endl;
 	cin >> name;
+	cin.ignore();
 	cout << "Hello " << name << endl;
-
+	cout << "What do you do?" << endl;
+	getline(cin, ans);
+	cout << "You are a " << ans << ", thats awesome!" << endl;
 	return 0;
 }
