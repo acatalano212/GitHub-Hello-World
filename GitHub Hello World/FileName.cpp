bbf5b14cf8c838198a92cb1095c0b5c1a481030a
acatalano212 <acatalano212@verizon.net>
2024-12-05 19:22:26 -0500

    added age question

--- GitHub Hello World/FileName.cpp
+++ GitHub Hello World/FileName.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
 
 	string name, ans;
+	int age;
 	cout << "What is your name?" << endl;
 	cin >> name;
 	cin.ignore();
@@ -13,5 +14,8 @@ int main() {
 	cout << "What do you do?" << endl;
 	getline(cin, ans);
 	cout << "You are a " << ans << ", thats awesome!" << endl;
+	cout << "How old are you?" << endl;
+	cin >> age;
+	cout << "Wow, you're only " << age << " years old!!!" << endl;
 	return 0;
 }
